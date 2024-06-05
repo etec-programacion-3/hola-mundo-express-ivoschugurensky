ea470775d28f08a8a878102548e03bafbe9e5f50
ivoschugurensky <i.schugurensky@alumno.etec.um.edu.ar>
2024-06-05 11:57:29 -0300

    holaProfe

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get('/:nombre', (req, res) => {
+  const nombre = req.params.nombre;
+  res.send(`Hola ${nombre}!`);
 });
 
 app.listen(3000, () => {
